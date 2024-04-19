470c6620725282827e08460afc2708a37e553978
Damian Dawidowski <damiandawidowski@gmail.com>
2024-04-19 15:57:54 +0200

    modified:   src/test/java/com/example/employeerolemanager/UserControllerTest.java

--- src/test/java/com/example/employeerolemanager/UserControllerTest.java
+++ src/test/java/com/example/employeerolemanager/UserControllerTest.java
@@ -1,13 +1,16 @@
 package com.example.employeerolemanager;
 
 import com.example.employeerolemanager.dtos.RegisterUserDto;
+import com.example.employeerolemanager.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,6 +27,9 @@ public class UserControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private UserService userService;
+
     @Test
     public void testRegisterUser() throws Exception {
         RegisterUserDto userDto = new RegisterUserDto();
@@ -38,4 +44,29 @@ public class UserControllerTest {
                 .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.email").value("test@example.com")); 
     }
+
+    @Test
+    @WithMockUser(username = "admin", roles = {"ADMIN"})
+    public void testUpgradeToSenior() throws Exception {
+        String fullName = "DamianDD";
+        
+        RegisterUserDto userDto = new RegisterUserDto();
+        userDto.setEmail("test2@example.com");
+        userDto.setPassword("ds33sds");
+        userDto.setFullName(fullName);
+
+        mockMvc.perform(MockMvcRequestBuilders.put("/users/makeSenior/{fullName}", fullName)
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk());
+
+        // Verify that the method was called with the correct arguments
+        // Add additional verification if needed
+    }
+
+
+
+
+
+
+
 }
